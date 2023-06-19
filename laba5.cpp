5abc7b1175bd20d005f9a6ae8133790475fd2d9c
Katrin Bnk <yekaterinabanko@gmail.com>
2023-06-19 16:39:16 +0300

    с комментами

--- laba5.cpp
+++ laba5.cpp
@@ -3,16 +3,18 @@
 #include <bitset>
 #include <string>
 
-
+//сама функция смены битов
 void swapBits(unsigned long long int& n, int p1, int p2) {
-    int bit1 = (n >> p1) & 1;
+    int bit1 = (n >> p1) & 1;//получение значения бита 
     int bit2 = (n >> p2) & 1;
 
     if (bit1 != bit2) {
-        n ^= (1 << p1) | (1 << p2);
+        n ^= (1 << p1) | (1 << p2); 
+        //если значения битов не одинаковые, то меняем их местами («^» операция побитового исключающего или)
     }
 }
 
+//просто для красивого вывода
 void printBinary(unsigned long long int& n) {
     std::bitset<64> binary(n);
     std::string binary_str = binary.to_string();
@@ -27,12 +29,22 @@ void printBinary(unsigned long long int& n) {
 }
 
 int main(int argc, char* argv[]) {
+    //это тоже проверка корректности ввода
     if (argc != 4) {
         std::cout << "Usage: " << argv[0] << " <number> <position 1> <position 2>" << std::endl;
         return 1;
     }
+
     unsigned long long int n;
     int p1, p2;
+
+    /*
+    * если ввод идет не из командной строки, то функция main не будет получать аргументы
+    * а сами аргументы будут просто считаны с клавиатуры std::cin 
+    */
+
+
+    //проверка на дурака
     try {
         n = std::stoull(argv[1]);
         p1 = std::stoi(argv[2]);
@@ -48,6 +60,9 @@ int main(int argc, char* argv[]) {
         std::cout << "Invalid positions" << std::endl;
         return 1;
     }
+    //проверка закончилась
+
+    
 
     std::cout << "Original number in binary: ";
     printBinary(n);
